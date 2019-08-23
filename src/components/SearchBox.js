477e3e952af9c1a6f50e73537b0158700b07a642
hanthienduc <hanthienduc.96@gmail.com>
2019-08-23 15:33:42 +0900

    applying redux to project files

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,15 +1,15 @@
 import React from 'react';
-// we are using destruturing here ({}) -- allows us to grab the pops object and grab is properties. searchfiled is actually not used and will be removed in upcoming video
 
-const SearchBox = (searchChange) => {
-	return(
-		<div className="pa5 ba b--green bg-lightest-blue">
-			<input type ="search"
-			 placeholder ="search box"
-			 onChange={searchChange}
-			  />
-		 </div>
-	);
+const SearchBox = ({ searchfield, searchChange }) => {
+  return (
+    <div className='pa5 ba b--green bg-lightest-blue'>
+      <input
+        type="search"
+        placeholder="search robots"
+        onChange={searchChange}
+      />
+    </div>
+  );
 }
 
-export default SearchBox;
+export default SearchBox;		
